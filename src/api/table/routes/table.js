3caa6e2336b1d8d8fdea92976c3152254df6641e
Jimenez-Martinez-Victoria <edenvictoriajimenes@gmail.com>
2024-06-11 22:03:56 -0400

    fix:Added table order component [SEM-107]

--- src/api/table/routes/table.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * table router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::table.table');
--- src/api/table/services/table.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * table service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::table.table');
