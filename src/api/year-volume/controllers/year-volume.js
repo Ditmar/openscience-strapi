ceab397a26f06d832809760690bc183eefe29e45
Blanca Tirado <blanquitatiradocamargo@gmail.com>
2024-06-12 23:42:49 -0400

    first commit

---
+++ src/api/year-volume/controllers/year-volume.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * year-volume controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::year-volume.year-volume');
---
+++ src/api/year-volume/routes/year-volume.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * year-volume router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::year-volume.year-volume');
